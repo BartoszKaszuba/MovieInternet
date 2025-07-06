af0236825d4fb22030e58959fcc3214a41fd10ce
Bartosz Kaszuba <bartoszkasz2005@gmail.com>
2025-07-06 15:30:09 +0200

    Added header "Welcome back" to the screen

---
+++ first-screen-react/src/components/MuiTypography.tsx
@@ -0,0 +1,13 @@
+import { Typography } from '@mui/material';
+// implementing typography to know how to use headers
+// with snippet rafc we create a functional component
+
+
+export const MuiTypography = () => {
+  return (
+    <div><Typography variant = 'h1'> Welcome back</Typography></div>
+  );
+}
+
+
+
